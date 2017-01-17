6a48a26d0e99b3cfffd448980bc42dc08bf9606c
Franck Elombat <f.elombat@gmail.com>
2017-01-17 11:41:11 +0100

    Dev Local DB setup

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=yii2tutoblogdb',
+            'username' => 'yii2suser',
+            'password' => 'yii2pass',
             'charset' => 'utf8',
         ],
         'mailer' => [
