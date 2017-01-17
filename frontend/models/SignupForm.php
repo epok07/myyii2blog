242929b7c269e8d8ce40a3853c14dd23a6f6027f
Franck Elombat <f.elombat@gmail.com>
2017-01-17 12:22:12 +0100

    Update signup process by adding a default role to the registering user

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -53,6 +53,18 @@ class SignupForm extends Model
         $user->setPassword($this->password);
         $user->generateAuthKey();
         
-        return $user->save() ? $user : null;
+        //return $user->save() ? $user : null;
+
+        if ($user->save()) {
+
+            // the following three lines were added:
+            $auth = \Yii::$app->authManager;
+            $userRole = $auth->getRole('user');
+            $auth->assign($userRole, $user->getId());
+
+            return $user;
+        }
+
+        return null;
     }
 }
