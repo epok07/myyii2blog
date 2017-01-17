729ebf495fde308e508073108a35064604f8cf54
Franck Elombat <f.elombat@gmail.com>
2017-01-17 11:24:35 +0100

    Basic Setup: Pretty URLs

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -28,14 +28,19 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
+        // Other components
+
+          
+
     ],
     'params' => $params,
 ];
+ 
